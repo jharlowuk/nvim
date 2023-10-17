07c8a02d0ad987858228a26f71a850632804f5e1
John Harlow <info@jharlow.uk>
2023-10-17 12:58:48 -0700

    Added Octo

--- lua/user/plugins/leap.lua
+++ lua/user/plugins/leap.lua
@@ -3,6 +3,6 @@ return {{
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-	require('leap').add_default_mappings()
+	    require('leap').add_default_mappings()
     end
 }}
--- lua/user/plugins/typescript-tools.lua
+++ lua/user/plugins/typescript-tools.lua
@@ -1,5 +1,8 @@
 return {{
     "pmizio/typescript-tools.nvim",
     dependencies = {"nvim-lua/plenary.nvim", "neovim/nvim-lspconfig"},
-    opts = {}
+    opts = {},
+    config = function()
+      require("typescript-tools").setup()
+    end,
 }}
