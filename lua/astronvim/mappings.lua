d94c0316bb28a976dbaa172573e600b14b516f6d
John Harlow <info@jharlow.uk>
2024-03-23 18:35:46 -0700

    fix(buffer resize): easier to resize buffers on mac

--- lua/astronvim/mappings.lua
+++ lua/astronvim/mappings.lua
@@ -213,10 +213,10 @@ if is_available "smart-splits.nvim" then
   maps.n["<C-j>"] = { function() require("smart-splits").move_cursor_down() end, desc = "Move to below split" }
   maps.n["<C-k>"] = { function() require("smart-splits").move_cursor_up() end, desc = "Move to above split" }
   maps.n["<C-l>"] = { function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" }
-  maps.n["<C-Up>"] = { function() require("smart-splits").resize_up() end, desc = "Resize split up" }
-  maps.n["<C-Down>"] = { function() require("smart-splits").resize_down() end, desc = "Resize split down" }
-  maps.n["<C-Left>"] = { function() require("smart-splits").resize_left() end, desc = "Resize split left" }
-  maps.n["<C-Right>"] = { function() require("smart-splits").resize_right() end, desc = "Resize split right" }
+  maps.n["<M-k>"] = { function() require("smart-splits").resize_up() end, desc = "Resize split up" }
+  maps.n["<M-j>"] = { function() require("smart-splits").resize_down() end, desc = "Resize split down" }
+  maps.n["<M-h>"] = { function() require("smart-splits").resize_left() end, desc = "Resize split left" }
+  maps.n["<M-l>"] = { function() require("smart-splits").resize_right() end, desc = "Resize split right" }
 else
   maps.n["<C-h>"] = { "<C-w>h", desc = "Move to left split" }
   maps.n["<C-j>"] = { "<C-w>j", desc = "Move to below split" }
